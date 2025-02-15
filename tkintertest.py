1b4b7612e89fe65d290f603fd0cea3de4209247d
Samuka-pixel <shark06778@sharkcoders.pt>
2025-02-15 10:32:43 +0000

    Add files via upload

---
+++ tkintertest.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from tkinter import ttk
+
+frame = Tk()
+frame.title("JACK IN A BOX")
+frame.geometry("400x400+700+300")
+frame.wm_resizable(width=False, height=False)
+frame.mainloop()
